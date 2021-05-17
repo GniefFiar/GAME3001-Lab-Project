912f37599b9891e6ab521469e71840822dc0ddcc
GniefFiar <ericzhou1717@gmail.com>
2021-05-16 21:04:05 -0400

    Created project

---
+++ src/PlayScene.cpp
@@ -0,0 +1,147 @@
+#include "PlayScene.h"
+#include "Game.h"
+#include "EventManager.h"
+
+// required for IMGUI
+#include "imgui.h"
+#include "imgui_sdl.h"
+#include "Renderer.h"
+#include "Util.h"
+
+PlayScene::PlayScene()
+{
+	PlayScene::start();
+}
+
+PlayScene::~PlayScene()
+= default;
+
+void PlayScene::draw()
+{
+	drawDisplayList();
+	SDL_SetRenderDrawColor(Renderer::Instance().getRenderer(), 255, 255, 255, 255);
+}
+
+void PlayScene::update()
+{
+	updateDisplayList();
+}
+
+void PlayScene::clean()
+{
+	removeAllChildren();
+}
+
+void PlayScene::handleEvents()
+{
+	EventManager::Instance().update();
+
+	// handle player movement with GameController
+	if (SDL_NumJoysticks() > 0)
+	{
+		if (EventManager::Instance().getGameController(0) != nullptr)
+		{
+			const auto deadZone = 10000;
+			if (EventManager::Instance().getGameController(0)->LEFT_STICK_X > deadZone)
+			{
+				m_pPlayer->setAnimationState(PLAYER_RUN_RIGHT);
+				m_playerFacingRight = true;
+			}
+			else if (EventManager::Instance().getGameController(0)->LEFT_STICK_X < -deadZone)
+			{
+				m_pPlayer->setAnimationState(PLAYER_RUN_LEFT);
+				m_playerFacingRight = false;
+			}
+			else
+			{
+				if (m_playerFacingRight)
+				{
+					m_pPlayer->setAnimationState(PLAYER_IDLE_RIGHT);
+				}
+				else
+				{
+					m_pPlayer->setAnimationState(PLAYER_IDLE_LEFT);
+				}
+			}
+		}
+	}
+
+
+	// handle player movement if no Game Controllers found
+	if (SDL_NumJoysticks() < 1)
+	{
+		if (EventManager::Instance().isKeyDown(SDL_SCANCODE_A))
+		{
+			m_pPlayer->setAnimationState(PLAYER_RUN_LEFT);
+			m_playerFacingRight = false;
+		}
+		else if (EventManager::Instance().isKeyDown(SDL_SCANCODE_D))
+		{
+			m_pPlayer->setAnimationState(PLAYER_RUN_RIGHT);
+			m_playerFacingRight = true;
+		}
+		else
+		{
+			if (m_playerFacingRight)
+			{
+				m_pPlayer->setAnimationState(PLAYER_IDLE_RIGHT);
+			}
+			else
+			{
+				m_pPlayer->setAnimationState(PLAYER_IDLE_LEFT);
+			}
+		}
+	}
+	
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_ESCAPE))
+	{
+		TheGame::Instance().quit();
+	}
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_1))
+	{
+		TheGame::Instance().changeSceneState(START_SCENE);
+	}
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_2))
+	{
+		TheGame::Instance().changeSceneState(END_SCENE);
+	}
+}
+
+void PlayScene::start()
+{
+	// Set GUI Title
+	m_guiTitle = "Play Scene";
+	
+	// Player Sprite
+	m_pPlayer = new Player();
+	addChild(m_pPlayer);
+	m_playerFacingRight = true;
+
+	ImGuiWindowFrame::Instance().setGUIFunction(std::bind(&PlayScene::GUI_Function, this));
+}
+
+void PlayScene::GUI_Function() const
+{
+	// Always open with a NewFrame
+	ImGui::NewFrame();
+
+	// See examples by uncommenting the following - also look at imgui_demo.cpp in the IMGUI filter
+	//ImGui::ShowDemoWindow();
+	
+	ImGui::Begin("Lab 1b", NULL, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_MenuBar);
+
+	static float position[2] = { 400.0f, 300.0f};
+	if(ImGui::SliderFloat2("Position", position, 0.0f, 800.0f))
+	{
+		m_pPlayer->getTransform()->position = glm::vec2(position[0], position[1]);
+		
+		std::cout << position[0] << std::endl;
+		std::cout << position[1] << std::endl;
+		std::cout << "---------------------------\n";
+	}
+	
+	ImGui::End();
+}
